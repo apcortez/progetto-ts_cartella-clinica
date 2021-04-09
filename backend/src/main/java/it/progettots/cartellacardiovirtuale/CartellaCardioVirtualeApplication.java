412b667eb8bd5f5a741f17daf8fd86c232ad2295
apcortez <a.cortez@campus.unimib.it>
2021-04-09 17:30:34 +0200

    config

---
+++ backend/src/main/java/it/progettots/cartellacardiovirtuale/CartellaCardioVirtualeApplication.java
@@ -0,0 +1,13 @@
+package it.progettots.cartellacardiovirtuale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartellaCardioVirtualeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartellaCardioVirtualeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/it/progettots/cartellacardiovirtuale/CartellaCardioVirtualeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.progettots.cartellacardiovirtuale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartellaCardioVirtualeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
