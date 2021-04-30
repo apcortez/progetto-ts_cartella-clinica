db29e4843595b8e6133aa9fd147bb74016c01483
apcortez <a.cortez@campus.unimib.it>
2021-04-30 13:10:26 +0200

    v1

--- cartellacardiovirtuale/src/main/java/it/progettots/cartellacardiovirtuale/CartellacardiovirtualeApplication.java
+++ cartellacardiovirtuale/src/main/java/it/progettots/cartellacardiovirtuale/CartellacardiovirtualeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CartellaCardioVirtualeApplication {
+public class CartellacardiovirtualeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CartellaCardioVirtualeApplication.class, args);
+		SpringApplication.run(CartellacardiovirtualeApplication.class, args);
 	}
 
 }
