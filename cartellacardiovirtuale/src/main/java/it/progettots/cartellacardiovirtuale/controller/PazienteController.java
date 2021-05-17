0a8aeee8afdba297a5795c04b0a097495b875741
apcortez <a.cortez@campus.unimib.it>
2021-05-17 13:57:48 +0200

    config paz

---
+++ cartellacardiovirtuale/src/main/java/it/progettots/cartellacardiovirtuale/controller/PazienteController.java
@@ -0,0 +1,44 @@
+package it.progettots.cartellacardiovirtuale.controller;
+
+import java.util.List;
+
+import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import it.progettots.cartellacardiovirtuale.entity.Utente;
+import it.progettots.cartellacardiovirtuale.service.UtenteService;
+
+@Controller
+@RequestMapping("/pazienti")
+public class PazienteController {
+	
+	@Autowired
+	private UtenteService utenteService;
+	
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}	
+	
+	
+	@GetMapping("/list")
+	public String listPazienti(Model theModel) {
+		List<Utente> thePazienti = utenteService.findByRole_Medico();
+		return "pazienti/list-pazienti";
+	}
+	
+
+	
+}
